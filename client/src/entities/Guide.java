22498738f46507b26434b5e27cde511e706facbb
onatan <ohadnatan@gmail.com>
2020-12-19 15:23:18 +0200

    added atrributes

--- client/src/entities/Guide.java
+++ client/src/entities/Guide.java
@@ -2,8 +2,45 @@ package entities;
 
 public class Guide extends Visitor {
 
+	private int SubscriptorNum;
+	private int VisitorNumers;
+	private int CreditCardNum;
+	
 	public Guide() {
 		// TODO Auto-generated constructor stub
 	}
+
+	public Guide(int subscriptorNum, int visitorNumers, int creditCardNum) {
+		super();
+		SubscriptorNum = subscriptorNum;
+		VisitorNumers = visitorNumers;
+		CreditCardNum = creditCardNum;
+	}
+
+	public int getSubscriptorNum() {
+		return SubscriptorNum;
+	}
+
+	public void setSubscriptorNum(int subscriptorNum) {
+		SubscriptorNum = subscriptorNum;
+	}
+
+	public int getVisitorNumers() {
+		return VisitorNumers;
+	}
+
+	public void setVisitorNumers(int visitorNumers) {
+		VisitorNumers = visitorNumers;
+	}
+
+	public int getCreditCardNum() {
+		return CreditCardNum;
+	}
+
+	public void setCreditCardNum(int creditCardNum) {
+		CreditCardNum = creditCardNum;
+	}
+	
+	
 
 }
