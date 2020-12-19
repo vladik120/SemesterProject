0c8d5a34c8aa2a44c4c8200f7cfa6530151882f1
onatan <ohadnatan@gmail.com>
2020-12-19 18:11:55 +0200

    LoginController

--- client/src/controllers/LoginController.java
+++ client/src/controllers/LoginController.java
@@ -1,9 +1,34 @@
 package controllers;
 
-public class LoginController {
+import entities.Visitor;
+import controllers.DataManager;
 
-	public LoginController() {
-		// TODO Auto-generated constructor stub
+public class LoginController extends AbstractController{
+	private int id;
+	private String username = "";
+	private String password = "";
+	private DataManager DM = DataManager.getDataManager();
+	private String type;
+	
+	public void changeScreen() {
+		//TODO
+	}
+	
+	public void loginValidation() {
+		if(type.equals("Visitor")) {
+			//TODO: validate if Guide/Family/Solo
+			Visitor current = new Visitor();
+			DM.setCurrentUser(current);
+		}
+		else {
+			
+		}
+		
+	}
+	
+	@Override
+	public Object RunQueryOnDB(Object id) {
+		//TODO: adding query
 	}
 
 }
